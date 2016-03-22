338fb20ec20c99353fde76e592ff7b36491397d7
icarrera <irismcarrera@gmail.com>
2016-03-21 18:16:19 -0700

    created a security.py module.

---
+++ learning_journal/security.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from pyramid.security import Allow, Everyone
+from pyramid.security import ALL_PERMISSIONS
+
+def DefaultRoot(object):
