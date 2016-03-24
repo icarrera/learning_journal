42f4b37b634919dfe75737568f63c80e7d0f705e
icarrera <irismcarrera@gmail.com>
2016-03-24 00:33:04 -0700

    JournalForm and LoginFom now inherit from SuperSecureForm

--- learning_journal/form.py
+++ learning_journal/form.py
@@ -1,15 +1,16 @@
-from wtforms import Form, StringField, validators, TextAreaField
+from wtforms import StringField, validators, TextAreaField
+from .security import SuperSecureForm
 
 
-class JournalForm(Form):
-
+class JournalForm(SuperSecureForm):
     """Create a form for our learning journal entries."""
+
     title = StringField('title', [validators.Length(min=1, max=128)])
     text = TextAreaField('text')
 
 
-class LoginForm(Form):
-
+class LoginForm(SuperSecureForm):
     """Create a login form for user authentication."""
+
     username = StringField('username', [validators.Length(min=1)])
     password = StringField('password', [validators.Length(min=5)])
